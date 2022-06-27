d57fe3b047dad1555eb60747c628827631aa96ca
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2022-06-27 20:38:31 +0400

    Homework 3

--- Homework3/main.js
+++ Homework3/main.js
@@ -33,3 +33,53 @@ function findMissingNumbers(arr){
 console.log(findMissingNumbers([1,2,4,5,8,11]));
 
 
+/*3Given the data array, you must get from data the result, as an example.*/
+/*// result
+[
+    {id: 1, name: ['a', 'c', 'd', 'g', 'l', 'n', 'p']},
+    { id: 2, name: [ 'b', 'f', 'i' ] },
+    { id: 3, name: [ 'e', 'm' ] },
+    { id: 6, name: [ 'h' ] },
+    { id: 4, name: [ 'j', 'q' ] },
+    { id: 7, name: [ 'k', 'r' ] },
+    { id: 8, name: [ 'o' ] }
+];*/
+const data = [
+  { id: 1, name: "a" },
+  { id: 2, name: "b" },
+  { id: 1, name: "c" },
+  { id: 1, name: "d" },
+  { id: 3, name: "e" },
+  { id: 2, name: "f" },
+  { id: 1, name: "g" },
+  { id: 6, name: "h" },
+  { id: 2, name: "i" },
+  { id: 4, name: "j" },
+  { id: 7, name: "k" },
+  { id: 1, name: "l" },
+  { id: 3, name: "m" },
+  { id: 1, name: "n" },
+  { id: 8, name: "o" },
+  { id: 1, name: "p" },
+  { id: 4, name: "q" },
+  { id: 7, name: "r" },
+];
+
+function groupById(data) {
+  const returnValue = [];
+  returnValue.push({ id: data[0].id, name: [data[0].name] });
+  for (let i = 1; i < data.length; i++) {
+    for (let j = 0; j < returnValue.length; j++) {
+      if (returnValue[j] && returnValue[j].id == data[i].id) {
+        returnValue[j].name.push(data[i].name);
+        break;
+      }
+      if (j == returnValue.length - 1) {
+        returnValue.push({ id: data[i].id, name: [data[i].name] });
+      }
+    }
+  }
+  return returnValue;
+}
+
+console.log(groupById(data));
